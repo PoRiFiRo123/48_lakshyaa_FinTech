ae56fa28ad5df3ec41af4a2799f7ca82dbef7621
PoRiFiRo〥#0001 <104781994+PoRiFiRo123@users.noreply.github.com>
2024-11-28 08:05:42 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'diligent-gnu-393.convex.cloud',
+        port: '',
+        pathname: '/account123/**',
+      },
+    ],
+  },
+}
